9521a01aa566c1b1df3df4f952512d5eb6f1982d
Ingo Ruhnke <grumbel@gmail.com>
2018-06-17 21:15:55 +0200

    Make use of project(... VERSION ...)

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv)
   QApplication app(argc, argv);
 
   app.setApplicationName("evtest-qt");
-  app.setApplicationVersion("0.1");
+  app.setApplicationVersion(PROJECT_VERSION);
   app.setWindowIcon(QIcon::fromTheme("evtest-qt"));
 
   std::vector<QString> args;

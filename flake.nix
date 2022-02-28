a6049a4bea151c2999f750fc9269936cc3093e94
Ingo Ruhnke <grumbel@gmail.com>
2022-02-28 13:34:16 +0100

    Add Nix flake build files

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A simple input device tester for Linux";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          evtest-qt = pkgs.stdenv.mkDerivation {
+            pname = "evtest-qt";
+            version = "0.2.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkgconfig
+              pkgs.qt5.wrapQtAppsHook
+            ];
+            buildInputs = [
+              pkgs.qt5.qtbase
+            ];
+           };
+        };
+        defaultPackage = packages.evtest-qt;
+      });
+}
